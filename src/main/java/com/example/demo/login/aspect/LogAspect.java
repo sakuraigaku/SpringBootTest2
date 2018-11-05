2d5d888e3efc0c5129e4879befd2c56e6081cc23
sakuraigaku <sakuraigaku009@gmail.com>
2018-11-05 23:54:05 +0900

    全画面のAOP

--- src/main/java/com/example/demo/login/aspect/LogAspect.java
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LogAspect {
-	@Before("execution(* com.example.demo.login.controller.LoginController.getLogin(..))")
+	@Before("execution(* *..*.*Controller.*(..))")
 	public void startLog(JoinPoint jp) {
 		System.out.println("メソッド開始："+jp.getSignature());
 	}
-	@After("execution(* com.example.demo.login.controller.LoginController.getLogin(..))")
+	@After("execution(* *..*.*Controller.*(..))")
 	public void endLog(JoinPoint jp) {
 		System.out.println("メソッド終了："+jp.getSignature());
 	}
